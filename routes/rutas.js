ef495fa168dcb731d1598cf5b736d115fca0a71d
401 <naime2008@hotmail.com>
2022-04-19 11:31:43 -0500

    AVANZADA- Se finalizan servicios de rutas

--- routes/rutas.js
+++ routes/rutas.js
@@ -14,9 +14,40 @@ rutas.get('/api/v1/habitaciones/', function (req, res) {
     res.send('Hello World1')
 })
 
-//3.Servicio para bsucar por ID
+//3.Servicio para buscar por ID
 rutas.get('/api/v1/habitaciones/id/', function (req, res) {
     res.send('Hello World')
 })
+
+//4.Servicio para cambiar habitaciones
+rutas.put('/api/v1/habitaciones/', function (req, res) {
+    res.send('Hello World')
+})
+
+//5. Servicio para Eliminar Habitaciones
+rutas.delete('/api/v1/habitaciones/', function (req, res) {
+    res.send('Hello World')
+})
+
+//6. Servicio para Ingresar reserva
+rutas.get('/api/v1/reserva/', function (req, res) {
+    res.send('Hello World')
+})
+
+//7. Servicio buscar reserva
+rutas.get('/api/v1/reserva/id/', function (req, res) {
+    res.send('Hello World')
+})
+
+//8. Servicio para editar reserva
+rutas.put('/api/v1/reserva/', function (req, res) {
+    res.send('Hello World')
+})
+
+//9. Servicio para elimiar reserva
+rutas.delete('/api/v1/reserva/', function (req, res) {
+    res.send('Hello World')
+})
+
 
 //Rutas para los servicios de reserva
